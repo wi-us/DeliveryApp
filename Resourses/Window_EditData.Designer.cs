3602fd64b45842af148cd1c86a4237f26b0f3eb8
wius <wius.dv@gmail.com>
2024-01-23 22:16:01 +0100

    +icon +lnk launch file

--- Resourses/Window_EditData.Designer.cs
+++ Resourses/Window_EditData.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Window_EditData));
             SuspendLayout();
             // 
             // Window_EditData
@@ -35,6 +36,7 @@
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(334, 450);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Window_EditData";
             Text = "Window_EditData";
             ResumeLayout(false);
